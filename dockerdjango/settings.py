5ddececea0069fdd150c800e8edac7fe7a7360ea
Evgeniy Zaluzhanskiy <ezaluzhanskiy@ozon.ru>
2022-09-10 18:16:06 +0300

    Add docker workflow

--- dockerdjango/settings.py
+++ dockerdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5wwnmc%)*-+z0y44^&zrga+xsuu25fo=w2v@7v!-x+)@w8c9=m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -73,23 +73,23 @@ WSGI_APPLICATION = 'dockerdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": os.environ.get("DB_NAME", "postgres"),
-#         "USER": os.environ.get("DB_USER", "postgres"),
-#         "PASSWORD": os.environ.get("DB_PASSWORD", "mysecretpassword"),
-#         "HOST": os.environ.get("DB_HOST", "db"),
-#         "PORT": "5432",
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
+        "HOST": os.environ.get("DB_HOST", "db"),
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
@@ -129,4 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
